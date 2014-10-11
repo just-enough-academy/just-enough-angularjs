cd5b0db37a16572ced84d6f163e45c0c15f59132
David Rogers <al.the.x@gmail.com>
2014-10-11 18:20:59 +0000

    Generated by Yeoman...

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name workspaceApp
+ * @description
+ * # workspaceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jeacademy.app', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
